8f05c76e253cf671b85bcbf335a1b0b35c14ad3a
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-03-23 23:13:02 +0700

    mini refactor

---
+++ src/components/Track/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Track = ({albumImage, songName, albumName, artistName }) => {
+  return (
+    <div>
+      <img src={albumImage} alt="albumImage" width={250} height={250}  className="album-image"/>
+      <h3>{songName}</h3>
+      <h4>{albumName}</h4>
+      <h5>{artistName}</h5>
+      <button type="button">
+        PLAY
+      </button>
+    </div>
+  )
+}
+
+export default Track
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,13 @@
 import React from 'react'
+import Track from '../../components/Track'
 import data from '../../utils/data'
 import './style.css'
 
 const Home = () => {
-  console.log(data.album.artists)
   return (
     <div class="info">
-        <img src={data.album.images[0].url} alt="albumImage" className="album-image"/>
-        <h3>{data.album.name}</h3>
-        <h4>{data.album.artists.name}</h4>
-        <h5>{data.name}</h5>
-        <button type="button">
-          PLAY
-        </button>
-      </div>
+      <Track albumImage={data.album.images[0].url} albumName={data.album.artists.name} songName={data.album.name} artistName={data.name}/>
+    </div>
   )
 }
 
